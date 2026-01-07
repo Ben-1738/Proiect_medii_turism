dd35d1a583aad2513da2bf1f86714363a28cccaf
Ben-1738 <bonchisbeniamin@yahoo.ro>
2026-01-07 17:59:45 +0200

    clase

---
+++ Proiect_medii_turism/Models/Client.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proiect_medii_turism.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [Phone(ErrorMessage = "Invalid phone number.")]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+
+        public ICollection<Booking> Bookings { get; set; }
+    }
+}
---
+++ Proiect_medii_turism/Models/TourPackage.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proiect_medii_turism.Models
+{
+    public class TourPackage
+    {
+        public int TourPackageId { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Required]
+        public string Destination { get; set; }
+        [Range(0, 50000, ErrorMessage = "Price must be a positive number.")]
+        public decimal Price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Leaving { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Returning { get; set; }
+    }
+}
